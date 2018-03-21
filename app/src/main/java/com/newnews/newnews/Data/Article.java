ebce418e3d1de983209560e821528bdaf9c24fb5
Philip Allanson <philip.allanson@gmail.com>
2018-03-21 15:14:33 +0100

    Added ArticleTest for easier Firebase testing

--- app/src/main/java/com/newnews/newnews/Data/Article.java
+++ app/src/main/java/com/newnews/newnews/Data/Article.java
@@ -1,16 +1,34 @@
 package com.newnews.newnews.Data;
 
+import java.util.Date;
+
 public class Article {
+    private String imgUrl;
     private String title;
     private String author;
+    private String organization;
+    private String content;
+    private Date dateCreated;
 
     public Article() {
         // Empty constructor is required for Firebase function.
     }
 
-    public Article(String title, String author) {
+    public Article(String imgUrl, String title, String author, String organization, String content, Date dateCreated) {
+        this.imgUrl = imgUrl;
         this.title = title;
         this.author = author;
+        this.organization = organization;
+        this.content = content;
+        this.dateCreated = dateCreated;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public String getTitle() {
@@ -27,5 +45,29 @@ public class Article {
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
     }
 }
